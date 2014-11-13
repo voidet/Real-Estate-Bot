79973212b1807d323044882e913ec7cf9710819b
Richard S <voidet@nightgen.com>
2014-11-13 18:23:30 +1000

    Initial Commit

---
+++ REABot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  REABot
+//
+//  Created by Richard S on 13/11/2014.
+//  Copyright (c) 2014 Wotif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
